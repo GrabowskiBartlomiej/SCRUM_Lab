1f13bd3cc115d19e73fee1d4a76c5757570a4e02
GrabowskiBartlomiej <bartlomiejgrabowski@virgilio.it>
2021-02-01 19:42:26 +0100

    strony kontakt oraz o nas dzialaja i wyswietlaja wygenerowana tresc lorem w takiej formie jaka mialby normalny tekst

--- src/main/java/pl/coderslab/web/About.java
+++ src/main/java/pl/coderslab/web/About.java
@@ -1,27 +1,20 @@
 package pl.coderslab.web;
 
-import pl.coderslab.dao.AdminDao;
-import pl.coderslab.dao.BookDao;
-import pl.coderslab.model.Admin;
-import pl.coderslab.model.Book;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
-/**
- * Do not change servlet address !!!
- */
-@WebServlet("")
-public class HomeServlet extends HttpServlet {
+@WebServlet(name = "About",urlPatterns = "/about")
+public class About extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-     
-        getServletContext().getRequestDispatcher("/home.jsp").forward(request, response);
+        getServletContext().getRequestDispatcher("/about.jsp").forward(request, response);
     }
 }
